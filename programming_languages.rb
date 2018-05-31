c094862b05f1531a902a0d73e85db9920dc65028
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-31 20:11:51 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -21,7 +21,16 @@ end
 
 def reformat_languages(languages)
   new_hash = {}
-  languages,each do |style, language|
-
-end
+  languages,each do |style, language_hash|
+    language_hash.each do |language, value_hash|
+      if new_hash[language].nil?
+        new_hash[language] = value_hash
+        new_hash[language][:style] = [style]
+      elsif !new_hash[language].nil?
+        new_hash[language][:style] << style
+      
+      end
+    end
+  end
+  new_hash
 end    
